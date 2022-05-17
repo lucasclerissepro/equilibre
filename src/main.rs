bbac94ef23d5385dd022794ca6e3af6e14dd6193
lucasclerisse <lucasclerisse@protonmail.com>
2022-05-18 00:18:38 +0800

    chore: add misc files

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,21 @@ use std::fmt::{Display, Formatter};
 
 #[derive(Debug)]
 pub struct CLI {
-    pub name: String
+    pub name: String,
 }
 
 impl Display for CLI {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.write_str(&self.name)?;
         Ok(())
+    }
+}
+
+mod tests {
+    #[test]
+    pub fn useless_test() {
+        assert!(true);
+        assert!(true);
     }
 }
 
